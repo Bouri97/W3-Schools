6ad97f39b90fd5b56ac57d9c96f77b9947841126
Rick Bouckaert <106081095+Bouri97@users.noreply.github.com>
2023-06-14 16:49:33 +0200

    Update Connection Binance.py

--- Connection Binance.py
+++ Connection Binance.py
@@ -1,8 +1,8 @@
 # Connect Binance API into Python
 
 from binance.client import Client
-client = Client("vP795eN3zGvFcT9iE30mDACus7Qa8TLHKQ51JxPbn4u7CtEkmCzinRal0bQAYxOj",
-                "YsHJqtrb0eMadgr1Y8qI3Fq2ZjxSwNqmZZXk2qWi8RncNBF5X0DbWGGTX3buQKgy")
+client = Client("v...j",
+                "Y..y")
 
 balance = client.get_asset_balance(asset="USDT")
 print(balance)
