dddf443fb0b7dc80da6c669c11e9079d8f03fb58
Rick Bouckaert <106081095+Bouri97@users.noreply.github.com>
2023-06-14 16:49:08 +0200

    Update Connection Bybiti.py

--- Connection Bybiti.py
+++ Connection Bybiti.py
@@ -3,5 +3,5 @@ import ccxt
 exchange_id = "Bybit"
 exchange_class = (getattr(ccxt, exchange_id))
 exchange = exchange_class({
-    "apikey" = "mGgNwDLzmzgNz75y7J"
-    "apisecret" = "JJEKR9RSg2acYL7ZUndfZaAA2y4LY4vNhDA5"})
+    "apikey" = "..."
+    "apisecret" = "..."})
